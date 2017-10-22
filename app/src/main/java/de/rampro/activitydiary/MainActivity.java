6cda2138ad4f5b689237484e43a33e0c946e1f5e
Raphael Mack <ramack@raphael-mack.de>
2017-10-22 20:27:20 +0200

    starting...

---
+++ app/src/main/java/de/rampro/activitydiary/MainActivity.java
@@ -0,0 +1,31 @@
+/*
+ * ActivityDiary
+ *
+ * Copyright (C) $today.year-$today.year Raphael Mack http://www.raphael-mack.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.rampro.activitydiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
