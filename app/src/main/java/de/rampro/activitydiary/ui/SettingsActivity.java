96a7c095309bda0a7c026c02117b25b7618c1e49
Raphael Mack <ramack@raphael-mack.de>
2017-11-12 13:42:42 +0100

    add settings, first step for #11

---
+++ app/src/main/java/de/rampro/activitydiary/ui/SettingsActivity.java
@@ -0,0 +1,62 @@
+/*
+ * ActivityDiary
+ *
+ * Copyright (C) 2017 Raphael Mack http://www.raphael-mack.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.rampro.activitydiary.ui;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import de.rampro.activitydiary.BuildConfig;
+import de.rampro.activitydiary.R;
+
+public class AboutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View contentView = inflater.inflate(R.layout.activity_about, null, false);
+
+        setContent(contentView);
+        TextView aboutText = (TextView)findViewById(R.id.aboutTextView);
+
+        String appName = getResources().getString(R.string.app_name);
+        String contributors = getResources().getString(R.string.contributors);
+        String versionName = BuildConfig.VERSION_NAME;
+
+        String mergedAboutText = getResources().getString(R.string.about_text, appName, versionName, contributors);
+        aboutText.setText(Html.fromHtml(mergedAboutText));
+        aboutText.setMovementMethod(LinkMovementMethod.getInstance());
+
+        mDrawerToggle.setDrawerIndicatorEnabled(false);
+    }
+
+    @Override
+    public void onResume(){
+        mNavigationView.getMenu().findItem(R.id.nav_about).setChecked(true);
+        super.onResume();
+    }
+}
---
+++ app/src/main/java/de/rampro/activitydiary/ui/SettingsFragment.java
@@ -0,0 +1,10 @@
+package de.rampro.activitydiary.ui;
+
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by r on 11.11.17.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+}
