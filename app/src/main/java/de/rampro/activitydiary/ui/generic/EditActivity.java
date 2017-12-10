e997b879b38b80ff534d8207c3d97be0a9513551
Raphael Mack <ramack@raphael-mack.de>
2017-12-10 10:20:24 +0100

    remove warning

--- app/src/main/java/de/rampro/activitydiary/ui/generic/EditActivity.java
+++ app/src/main/java/de/rampro/activitydiary/ui/generic/EditActivity.java
@@ -22,7 +22,6 @@ import android.content.AsyncQueryHandler;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
