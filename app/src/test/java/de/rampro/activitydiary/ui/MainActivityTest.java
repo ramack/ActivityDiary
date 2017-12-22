2089b15b6977857825ee2f786eef270104094d66
Raphael Mack <ramack@raphael-mack.de>
2017-12-22 15:59:19 +0100

    add meaningful testing to show absense of #60

--- app/src/test/java/de/rampro/activitydiary/ui/MainActivityTest.java
+++ app/src/test/java/de/rampro/activitydiary/ui/MainActivityTest.java
@@ -20,7 +20,12 @@
 package de.rampro.activitydiary.ui;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,13 +33,22 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowApplication;
+import org.robolectric.shadows.ShadowLooper;
+import org.robolectric.shadows.ShadowToast;
+
+import java.util.concurrent.TimeUnit;
 
 import de.rampro.activitydiary.BuildConfig;
 import de.rampro.activitydiary.R;
+import de.rampro.activitydiary.helpers.ActivityHelper;
+import de.rampro.activitydiary.model.DiaryActivity;
 import de.rampro.activitydiary.ui.main.MainActivity;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.robolectric.Shadows.shadowOf;
 
@@ -58,6 +72,80 @@ public class MainActivityTest {
     }
 
     @Test
+    public void currentNullActivity() throws Exception {
+        /* show absense of #60 */
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+
+        View card = activity.findViewById(R.id.card);
+        assertNotNull("Current activity card available", card);
+
+        TextView nameView = (TextView) card.findViewById(R.id.activity_name);
+        assertNotNull("Current activity Text available", nameView);
+
+        /* initial creation shall not have any activity selected */
+        assertEquals(nameView.getText(), "<No Activity>");
+
+        assertNull(ActivityHelper.helper.getCurrentActivity());
+
+        FloatingActionButton fabNoteEdit = (FloatingActionButton) activity.findViewById(R.id.fab_edit_note);
+        FloatingActionButton fabAttachPicture = (FloatingActionButton) activity.findViewById(R.id.fab_attach_picture);
+
+        assertNotNull("we have two FABs", fabNoteEdit);
+        assertNotNull("we have two FABs", fabAttachPicture);
+
+        fabNoteEdit.performClick();
+
+        ShadowLooper.idleMainLooper(100, TimeUnit.MILLISECONDS);
+        assertEquals(ShadowToast.getTextOfLatestToast().toString(), "To perform this action it is necessary to select an activity first.");
+
+        fabAttachPicture.performClick();
+
+        ShadowLooper.idleMainLooper(100, TimeUnit.MILLISECONDS);
+        assertEquals(ShadowToast.getTextOfLatestToast().toString(), "To perform this action it is necessary to select an activity first.");
+
+    }
+
+    @Test
+    public void currentActivity() throws Exception {
+        /* now select an activity */
+        DiaryActivity someAct = new DiaryActivity(1, "Test", Color.BLACK);
+
+        ActivityHelper.helper.insertActivity(someAct);
+        assertNotNull(someAct);
+
+        ActivityHelper.helper.setCurrentActivity(someAct);
+        assertEquals(ActivityHelper.helper.getCurrentActivity(), someAct);
+
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+
+        View card = activity.findViewById(R.id.card);
+        TextView nameView = (TextView) card.findViewById(R.id.activity_name);
+        assertNotNull("Current activity Text available", nameView);
+
+        assertEquals(nameView.getText(), "Test");
+
+        FloatingActionButton fabNoteEdit = (FloatingActionButton) activity.findViewById(R.id.fab_edit_note);
+        FloatingActionButton fabAttachPicture = (FloatingActionButton) activity.findViewById(R.id.fab_attach_picture);
+
+        assertNotNull("we have two FABs", fabNoteEdit);
+        assertNotNull("we have two FABs", fabAttachPicture);
+
+        fabNoteEdit.performClick();
+
+        DialogFragment dialogFragment = (DialogFragment) activity.getSupportFragmentManager()
+                .findFragmentByTag("NoteEditDialogFragment");
+        assertNotNull(dialogFragment);
+
+        ShadowLooper.idleMainLooper(100, TimeUnit.MILLISECONDS);
+        assertNull(ShadowToast.getTextOfLatestToast());
+
+        fabAttachPicture.performClick();
+
+        ShadowLooper.idleMainLooper(100, TimeUnit.MILLISECONDS);
+        assertNull(ShadowToast.getTextOfLatestToast());
+    }
+
+    @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = activity;
@@ -65,16 +153,5 @@ public class MainActivityTest {
         assertEquals("de.rampro.activitydiary", appContext.getPackageName());
     }
 
-    @Test
-    public void createMainActivity() throws Exception {
-//        final MainActivity activity = Robolectric.setupActivity(MainActivity.class);
-
-        final Menu menu = shadowOf(activity).getOptionsMenu();
-        assertNotNull(activity);
-        assertNotNull(menu);
-
-        assertTrue(menu.findItem(R.id.action_add_activity).isEnabled());
-
-    }
 }
 
