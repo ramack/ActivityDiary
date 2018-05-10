66f8f9cf49ddaa7eb5818e2d2f0b3b3055cd2b6f
Raphael Mack <ramack@raphael-mack.de>
2018-05-10 19:31:00 +0200

    wip on searchable history

--- app/src/main/java/de/rampro/activitydiary/ui/history/HistoryActivity.java
+++ app/src/main/java/de/rampro/activitydiary/ui/history/HistoryActivity.java
@@ -20,6 +20,7 @@
 package de.rampro.activitydiary.ui.history;
 
 import android.app.LoaderManager;
+import android.app.SearchManager;
 import android.content.AsyncQueryHandler;
 import android.content.ContentValues;
 import android.content.Context;
@@ -29,8 +30,10 @@ import android.content.CursorLoader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -53,7 +56,7 @@ import de.rampro.activitydiary.ui.main.NoteEditDialog;
 public class HistoryActivity extends BaseActivity implements
         LoaderManager.LoaderCallbacks<Cursor>,
         NoteEditDialog.NoteEditDialogListener,
-        HistoryRecyclerViewAdapter.SelectListener {
+        HistoryRecyclerViewAdapter.SelectListener, SearchView.OnCloseListener, SearchView.OnQueryTextListener {
 
     private static final String[] PROJECTION = new String[] {
             ActivityDiaryContract.Diary.TABLE_NAME + "." + ActivityDiaryContract.Diary._ID,
@@ -70,6 +73,8 @@ public class HistoryActivity extends BaseActivity implements
 
     private HistoryRecyclerViewAdapter historyAdapter;
     private DetailRecyclerViewAdapter detailAdapters[];
+    private MenuItem searchMenuItem;
+    private SearchView searchView;
 
     @Override
     public void onItemClick(HistoryViewHolders viewHolder, int adapterPosition, int diaryID) {
@@ -84,6 +89,49 @@ public class HistoryActivity extends BaseActivity implements
         dialog.setText(viewHolder.mNoteLabel.getText().toString());
         dialog.show(getSupportFragmentManager(), "NoteEditDialogFragment");
         return true;
+    }
+
+    /**
+     * The user is attempting to close the SearchView.
+     *
+     * @return true if the listener wants to override the default behavior of clearing the
+     * text field and dismissing it, false otherwise.
+     */
+    @Override
+    public boolean onClose() {
+        filterHistoryView(null);
+        return false;
+    }
+
+    /**
+     * Called when the user submits the query. This could be due to a key press on the
+     * keyboard or due to pressing a submit button.
+     * The listener can override the standard behavior by returning true
+     * to indicate that it has handled the submit request. Otherwise return false to
+     * let the SearchView handle the submission by launching any associated intent.
+     *
+     * @param query the query text that is to be submitted
+     * @return true if the query has been handled by the listener, false to let the
+     * SearchView perform the default action.
+     */
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        filterHistoryView(query);
+
+        return false;
+    }
+
+    /**
+     * Called when the query text is changed by the user.
+     *
+     * @param newText the new content of the query text field.
+     * @return false if the SearchView should perform the default action of showing any
+     * suggestions if available, true if the action was handled by the listener.
+     */
+    @Override
+    public boolean onQueryTextChange(String newText) {
+// for now no dynamic change before starting the search...
+        return false;
     }
 
     protected class QHandler extends AsyncQueryHandler {
@@ -127,11 +175,36 @@ public class HistoryActivity extends BaseActivity implements
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        /* todo change menu */
-        /* TODO #25: ADD a search */
-/*        inflater.inflate(R.menu.manage_menu, menu);*/
+        inflater.inflate(R.menu.history_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchMenuItem = menu.findItem(R.id.action_filter);
+        searchView = (SearchView) searchMenuItem.getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setOnCloseListener(this);
+        searchView.setOnQueryTextListener(this);
+        // setOnSuggestionListener -> for selection of a suggestion
+        // setSuggestionsAdapter
+
         return true;
     }
+
+    /**
+     *
+     * @param query the search string, if null resets the filter
+     */
+    private void filterHistoryView(@Nullable String query){
+        Toast.makeText(this, "searching for " + query, Toast.LENGTH_LONG).show();
+        /*
+        this.filter = query;
+        selectAdapter = new SelectRecyclerViewAdapter(MainActivity.this, filtered);
+        selectRecyclerView.swapAdapter(selectAdapter, false);
+        */
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -239,5 +312,12 @@ public class HistoryActivity extends BaseActivity implements
         detailAdapters[adapter.getAdapterId()] = adapter;
         getLoaderManager().initLoader(adapter.getAdapterId(), b, this);
 
+    }
+
+    protected void onNewIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            filterHistoryView(query);
+        }
     }
 }
